b10fb661af33b81a985478e144d36946986d92d1
didal <didal.ru@gmail.com>
2019-12-29 21:50:26 +0400

    Добавил движение игрока по вертикали

--- venv/meteorite.py
+++ venv/meteorite.py
@@ -18,19 +18,30 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.speedy = 0
 
     def update(self):
         self.speedx = 0
+        self.speedy = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
             self.speedx = -8
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
+        if keystate[pygame.K_UP]:
+            self.speedy = -4
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 4
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
 
 
 # Класс для метеорита
