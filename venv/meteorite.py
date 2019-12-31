730628a140d89de9a3f5e7aab9cf87f344aca825
didal <didal.ru@gmail.com>
2019-12-31 16:01:19 +0400

    Второй вариант игры. Жизнь и здоровье

--- venv/meteorite.py
+++ venv/meteorite.py
@@ -16,10 +16,12 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width / 2)
-        self.rect.centerx = WIDTH /    2
+        self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
         self.speedy = 0
+        self.health = 100
+        self.lives = 5
 
     def update(self):
         self.speedx = 0
@@ -72,13 +74,13 @@ class Rocket(pygame.sprite.Sprite):
 class Meteorite(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = meteorite_img
+        self.image = random.choice(meteorite_images)
         self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width / 2)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(1, 8)
+        self.speedy = random.randrange(1, 7)
         self.speedx = random.randrange(-3, 3)
 
     def update(self):
@@ -90,7 +92,37 @@ class Meteorite(pygame.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 
+def print_text(canvas, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, (255, 255, 255))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    canvas.blit(text_surface, text_rect)
+
+
+def draw_lives(canvas, x, y, lives, img):
+    rect = pygame.Rect((x, y - 5, 30 * lives, 35))
+    pygame.draw.rect(canvas, (127, 255, 212), rect)
+    for i in range(lives):
+        img_rect = img.get_rect()
+        img.set_colorkey((0, 0, 0))
+        img_rect.x = x + 30 * i + 2
+        img_rect.y = y
+        canvas.blit(img, img_rect)
+
+
+def draw_health_bar(canvas, x, y, health):
+    if health < 0:
+        health = 0
+    fill = health / 100 * 100
+    outline_rect = pygame.Rect(x, y, 100, 15)
+    fill_rect = pygame.Rect(x, y, fill, 15)
+    pygame.draw.rect(canvas, (255, 165, 0), fill_rect)
+    pygame.draw.rect(canvas, (255, 255, 255), outline_rect, 2)
+
+
 img_dir = path.join(path.dirname(__file__), 'images')
+font_name = pygame.font.match_font('arial')
 # Создаем окно
 pygame.init()
 pygame.mixer.init()
@@ -102,10 +134,14 @@ background = pygame.image.load(path.join(img_dir, "space.png")).convert()
 background_rect = background.get_rect()
 # Космический истребитель
 player_img = pygame.image.load(path.join(img_dir, "ship.png")).convert()
-# Метеорит
-meteorite_img = pygame.image.load(path.join(img_dir, "meteorite.png")).convert()
+# Метеориты
+meteorite_images = []
+for i in '12345':
+    meteorite_images.append(pygame.image.load(path.join(img_dir, "meteorite0" + i + ".png")).convert())
 # Выстрел (ракета)
 rocket_img = pygame.image.load(path.join(img_dir, "rocket.png")).convert()
+# Жизнь
+live_img = pygame.image.load(path.join(img_dir, "ship_mini.png")).convert()
 #
 all_sprites = pygame.sprite.Group()
 meteorites = pygame.sprite.Group()
@@ -116,6 +152,7 @@ for i in range(9):
     meteorite = Meteorite()
     all_sprites.add(meteorite)
     meteorites.add(meteorite)
+rating = 0
 # Игровой цикл
 running = True
 while running:
@@ -131,15 +168,29 @@ while running:
     # Проверка попадания ракеты
     hits = pygame.sprite.groupcollide(meteorites, rockets, True, True)
     for hit in hits:
+        rating += 50 - hit.radius
         meteorite = Meteorite()
         all_sprites.add(meteorite)
         meteorites.add(meteorite)
     # Проверка столкновения метеоритов и игрока
-    hits = pygame.sprite.spritecollide(player, meteorites, False, pygame.sprite.collide_circle)
-    if hits:
+    hits = pygame.sprite.spritecollide(player, meteorites, True, pygame.sprite.collide_circle)
+    for hit in hits:
+        player.health -= hit.radius * 2
+        print(player.health, player.lives)
+        meteorite = Meteorite()
+        all_sprites.add(meteorite)
+        meteorites.add(meteorite)
+        if player.health <= 0:
+            player.lives -= 1
+            player.health = 100
+    # Если игрок умер
+    if player.lives == 0:
         running = False
     screen.fill((0, 0, 0))
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    print_text(screen, str(rating), 18, WIDTH / 2, 10)
+    draw_lives(screen, WIDTH - 150, 5, player.lives, live_img)
+    draw_health_bar(screen, 5, 5, player.health)
     pygame.display.flip()
 pygame.quit()
